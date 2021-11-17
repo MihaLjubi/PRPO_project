4383bae40db1253f49cd3ae5a30a8a582b4f9ea5
MihaLjubi <ml3635@student.uni-lj.si>
2021-11-17 16:03:28 +0100

    Belezenje klicev setup

---
+++ Lokacijski_opomniki/storitve/src/main/java/si/fri/prpo/polnilnice/interceptor/BelezenjeKlicevInterceptor.java
@@ -0,0 +1,10 @@
+package si.fri.prpo.polnilnice.interceptor;
+
+import javax.interceptor.Interceptor;
+import java.util.logging.Logger;
+
+@Interceptor
+@BeleziKlice
+public class BelezenjeKlicevInterceptor {
+
+}
---
+++ Lokacijski_opomniki/storitve/src/main/java/si/fri/prpo/polnilnice/zrna/BelezenjeKlicevZrno.java
@@ -0,0 +1,38 @@
+package si.fri.prpo.polnilnice.zrna;
+
+import si.fri.prpo.polnilnice.entitete.Rezervacija;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+import java.util.logging.Logger;
+
+public class BelezenjeKlicevZrno {
+
+    private Logger logger = Logger.getLogger(BelezenjeKlicevZrno.class.getName());
+
+    @PostConstruct
+    private void init() {
+        logger.info("Inicializacija zrna " + BelezenjeKlicevZrno.class.getName());
+    }
+
+    @PreDestroy
+    private void destroy() {
+        logger.info("Uničenje zrna " + BelezenjeKlicevZrno.class.getSimpleName());
+    }
+
+    int count = 0;
+
+    public void inc() {
+        count++;
+        logger.info("Number of calls: " + count);
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+}
